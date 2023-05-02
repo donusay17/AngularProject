c33287bd8703b385135fa2234144af384f6ac3aa
donusay17 <83464038+donusay17@users.noreply.github.com>
2023-05-02 23:45:30 +0300

    first commit

---
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from 'src/app/models/tutorial.model';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+@Component({
+  selector: 'app-add-tutorial',
+  templateUrl: './add-tutorial.component.html',
+  styleUrls: ['./add-tutorial.component.css']
+})
+export class AddTutorialComponent implements OnInit {
+  tutorial: Tutorial = {
+    title: '',
+    description: '',
+    published: false,
+    height: 0,
+    weight: 0
+  };
+  submitted = false;
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+  }
+
+  saveTutorial(): void {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description,
+      height: this.tutorial.height,
+      weight: this.tutorial.weight
+    };
+
+    this.tutorialService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newTutorial(): void {
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published: false,
+      height: 0,
+     weight: 0
+      
+    };
+  }
+
+}
